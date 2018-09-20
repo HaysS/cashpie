70966cc4c7dcbb71013b866da67986ded826cbe5
HaysS <stanfordhays@gmail.com>
2018-09-19 23:54:32 -0500

    comment out statusBarBackground code in sep component file

---
+++ src/components/StatusBarBackground.js
@@ -0,0 +1,14 @@
+// Delete this file if you don't end up encountering status bar background issues
+
+
+// # Save code below in case encountering status bar issues later #
+// const { StatusBarManager } = NativeModules;
+// const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBarManager.HEIGHT;
+
+
+
+// function StatusBarBackground(props) {
+//   return(
+//     <View style={[styles.statusBarBackground, props.style || {}]}></View>
+//   ) 
+// }
