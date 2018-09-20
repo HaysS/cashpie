789e34277f6bd864946b061ab1b20c446b7d3973
HaysS <stanfordhays@gmail.com>
2018-09-19 23:22:13 -0500

    Added src dir and added relevant files

--- App.js
+++ App.js
@@ -10,92 +10,11 @@ import {
   NativeModules,
   InteractionManager, } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
-
+import AppNavigator from './src/navigation/AppNavigator';
 
 //Sets dev or production config automatically
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config')[env];
-
-const { StatusBarManager } = NativeModules;
-
-//Screen measurements
-const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBarManager.HEIGHT;
-const { height, width } = Dimensions.get('window');
- 
-//Relevant for grid like keypad
-const col_count = 4;
-const box_width = width / col_count;
-
-function Button(props) {
-  return(
-    <TouchableOpacity onPress={() => { props.cb() }} style={styles.box}><Text style={styles.buttonText}>{props.text}</Text></TouchableOpacity>
-  )
-}
-
-function StatusBarBackground(props) {
-  return(
-    <View style={[styles.statusBarBackground, props.style || {}]}></View>
-  ) 
-}
-
-class Numpad extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: '234234'};
-  } 
-
-  addChar(input) {
-    console.log('input: ', input)
-    const text = "" + this.state.text + input;
-
-    InteractionManager.runAfterInteractions(() => {
-      this.setState({text});
-    })
-  }
-
-  clearText() {
-    InteractionManager.runAfterInteractions(() => {
-      this.setState({text: ''});
-    })
-  }
-
-  render() {
-    return(
-      <View style={{flex: 1}}>
-        <View style={styles.topRow}>
-          <Text style={styles.input}>{this.state.text}</Text>        
-        </View> 
-        <View style={{flex: 1}}>
-          <View style={styles.row}>
-            <Button text='1' cb={() => { this.addChar('1') }}/>         
-            <Button text='2' cb={() => { this.addChar('2') }}/>         
-            <Button text='3' cb={() => { this.addChar('3') }}/>         
-            <Button text='CLR' cb={() => { this.clearText() }}/>         
-          </View> 
-          <View style={styles.row}>
-            <Button text='4' cb={() => { this.addChar('4') }}/>         
-            <Button text='5' cb={() => { this.addChar('5') }}/>         
-            <Button text='6' cb={() => { this.addChar('6') }}/>         
-            <Button text='blaudnfk' cb={() => {}}/>         
-          </View> 
-          <View style={styles.row}>
-            <Button text='7' cb={() => { this.addChar('7') }}/>         
-            <Button text='8' cb={() => { this.addChar('8') }}/>         
-            <Button text='9' cb={() => { this.addChar('9') }}/>         
-            <Button text='BLEH' cb={() => {}}/>         
-          </View> 
-          <View style={styles.row}>
-            <Button text='0' cb={() => { this.addChar('0') }}/>         
-            <Button text='' cb={() => {}}/>         
-            <Button text='' cb={() => {}}/>         
-            <Button text='' cb={() => {}}/>      
-          </View> 
-        </View>
-      </View>
-    )
-  }
-}
 
 export default class App extends React.Component {
   state = {
@@ -124,15 +43,15 @@ export default class App extends React.Component {
   _loadResourcesAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
+        require('./src/assets/images/robot-dev.png'),
+        require('./src/assets/images/robot-prod.png'),
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+        'space-mono': require('./src/assets/fonts/SpaceMono-Regular.ttf'),
       }),
     ]);
   };
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,12 +5,96 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  StatusBar, 
+  Dimensions,
   TouchableOpacity,
+  NativeModules,
+  InteractionManager,
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
+
+const { StatusBarManager } = NativeModules;
+
+//Screen measurements
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBarManager.HEIGHT;
+const { height, width } = Dimensions.get('window');
+ 
+//Relevant for grid like keypad
+const col_count = 4;
+const box_width = width / col_count;
+
+function Button(props) {
+  return(
+    <TouchableOpacity onPress={() => { props.cb() }} style={styles.box}><Text style={styles.buttonText}>{props.text}</Text></TouchableOpacity>
+  )
+}
+
+function StatusBarBackground(props) {
+  return(
+    <View style={[styles.statusBarBackground, props.style || {}]}></View>
+  ) 
+}
+
+class Numpad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: '234234'};
+  } 
+
+  addChar(input) {
+    console.log('input: ', input)
+    const text = "" + this.state.text + input;
+
+    InteractionManager.runAfterInteractions(() => {
+      this.setState({text});
+    })
+  }
+
+  clearText() {
+    InteractionManager.runAfterInteractions(() => {
+      this.setState({text: ''});
+    })
+  }
+
+  render() {
+    return(
+      <View style={{flex: 1}}>
+        <View style={styles.topRow}>
+          <Text style={styles.input}>{this.state.text}</Text>        
+        </View> 
+        <View style={{flex: 1}}>
+          <View style={styles.row}>
+            <Button text='1' cb={() => { this.addChar('1') }}/>         
+            <Button text='2' cb={() => { this.addChar('2') }}/>         
+            <Button text='3' cb={() => { this.addChar('3') }}/>         
+            <Button text='CLR' cb={() => { this.clearText() }}/>         
+          </View> 
+          <View style={styles.row}>
+            <Button text='4' cb={() => { this.addChar('4') }}/>         
+            <Button text='5' cb={() => { this.addChar('5') }}/>         
+            <Button text='6' cb={() => { this.addChar('6') }}/>         
+            <Button text='blaudnfk' cb={() => {}}/>         
+          </View> 
+          <View style={styles.row}>
+            <Button text='7' cb={() => { this.addChar('7') }}/>         
+            <Button text='8' cb={() => { this.addChar('8') }}/>         
+            <Button text='9' cb={() => { this.addChar('9') }}/>         
+            <Button text='BLEH' cb={() => {}}/>         
+          </View> 
+          <View style={styles.row}>
+            <Button text='0' cb={() => { this.addChar('0') }}/>         
+            <Button text='' cb={() => {}}/>         
+            <Button text='' cb={() => {}}/>         
+            <Button text='' cb={() => {}}/>      
+          </View> 
+        </View>
+      </View>
+    )
+  }
+}
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
